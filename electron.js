fe88a74ba810d2b1361eb5093e30d7585334084a
Emendir <dev@emendir.tech>
2024-11-05 14:17:40 +0100

    Initialised react-electron app

---
+++ electron.js
@@ -0,0 +1,42 @@
+// electron.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+// Dynamically import electron-is-dev as it’s an ES module
+let isDev;
+(async () => {
+  isDev = (await import('electron-is-dev')).default;
+})();
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startURL = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '/public/index.html')}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
